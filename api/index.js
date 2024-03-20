26cc1c2249c58e17095e96f391873be0f311ed08
Aman <amankun77@gmail.com>
2024-03-21 02:36:33 +0530

    signup route

--- api/index.js
+++ api/index.js
@@ -2,14 +2,17 @@ import express from "express";
 import connectDB from "../db/index.js";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js"
 dotenv.config();
 const app = express();
+app.use(express.json());
 connectDB()
 .then(()=>{
     app.listen(3000,()=>{
         console.log("Server listening on port 3000");
     });
     app.use('/api/user',userRoutes);
+    app.use('/api/auth',authRoutes);
 })
 .catch((err)=>{
     console.log(err.message);
